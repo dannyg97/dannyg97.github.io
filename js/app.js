7e628ef2049dc41dd7c8f3a81b893ed2e292e51e
dannyg97 <38267274+dannyg97@users.noreply.github.com>
2019-07-27 14:31:02 +1000

    Created basic landing page; implemented typed.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".element").typed({
+        strings: ["Hello!", "Salut!", "Ciao!", "Olá!", "Bonjour!", "こんにちは!", "Hello!"],
+        typeSpeed: 40,
+        backSpeed: 20,
+        loop: false,
+        showCursor: false
+    });
+});
