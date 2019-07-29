0b7d1ba31a9a08ff083d927c910ec4b8215ea13b
Danny <38267274+dannyg97@users.noreply.github.com>
2019-07-29 16:29:32 +1000

    Navbar is now fixed + transition animations

--- js/app.js
+++ js/app.js
@@ -7,3 +7,34 @@ $(function(){
         showCursor: false
     });
 });
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
+
+$(document).ready(function (){
+    $("#about-nav").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function (){
+    $("#skills-nav").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function (){
+    $("#contact-nav").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+});
