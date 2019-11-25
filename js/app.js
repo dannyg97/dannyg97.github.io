b00dda2fef3977011acfc63656a963fb611617bc
Danny <38267274+dannyg97@users.noreply.github.com>
2019-11-26 09:35:45 +1100

    now comes with a loading screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+$(window).on('load', function(){
+    $('.loader-wrapper').fadeOut("slow");
+});
+
 $(function(){
     $(".element").typed({
         strings: ["Hello!", "Ciao!", "Olá!", "Bonjour!", "Hello!"],
